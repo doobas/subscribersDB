0d3bea4d63527afdd7f372cb3949ba537ca11e2f
Ramunas Pabreza <doobas@gmail.com>
2020-01-04 20:26:35 +0200

    Frontend initialized

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -46,7 +46,9 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        //
+        Route::fallback(function () {
+            return view('app');
+        });
     }
 
     /**
